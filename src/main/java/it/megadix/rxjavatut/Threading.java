ac3dd4ace3da89a12efdfda7a1a1f0e402b2c47c
Dimitri De Franciscis <ddefranciscis@byte-code.com>
2016-03-07 18:32:22 +0100

    Updated Threading example

--- src/main/java/it/megadix/rxjavatut/Threading.java
+++ src/main/java/it/megadix/rxjavatut/Threading.java
@@ -6,6 +6,7 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example inspired by "RxJava Threading Examples":
@@ -35,31 +36,85 @@ public class Threading {
         System.out.println("noThreads()");
 
         buildGenerator(5)
-                .doOnNext(debug("+ Generated\t"))
-                .map(shiftUp).doOnNext(debug("| Shifted Up"))
-                .map(shiftDown).doOnNext(debug("| Shifted Down"))
-                .subscribe(debug("+ Received\t"));
+                .doOnNext(debug("Generated\t"))
+                .map(shiftUp).doOnNext(debug("Shifted Up\t"))
+                .map(shiftDown).doOnNext(debug("Shifted Down"))
+                .subscribe(debug("Received\t"));
+
+        System.out.println("");
 
         return this;
     }
 
-    private Threading subscribeOn_IOScheduler() throws InterruptedException {
-        System.out.println("subscribeOn_IOScheduler()");
+    private Threading subscribeOn() throws InterruptedException {
+        System.out.println("subscribeOn()");
 
         // Since subscription now occurs on a different thread, the subscribe() call will return control to the thread
         // that called it almost immediately, so we need to wait until completion using a semaphore
         CountDownLatch latch = new CountDownLatch(1);
 
         buildGenerator(5)
-                .subscribeOn(Schedulers.io())
-                .doOnNext(debug("+ Generated\t"))
-                .map(shiftUp).doOnNext(debug("| Shifted Up"))
-                .map(shiftDown).doOnNext(debug("| Shifted Down"))
-                // decrement semaphore
+                .doOnNext(debug("Generated\t"))
+                .map(shiftUp).doOnNext(debug("Shifted Up\t"))
+                .map(shiftDown).doOnNext(debug("Shifted Down"))
+                // on completed, decrement countdown in latch
                 .doOnCompleted(latch::countDown)
-                .subscribe(debug("+ Received\t"));
+                // subscribe on a different thread
+                // NOTE: the position of this call does NOT influence threading
+                .subscribeOn(Schedulers.io())
+                .subscribe(debug("Received\t"));
 
         latch.await();
+
+        System.out.println("");
+
+        return this;
+    }
+
+    private Threading observeOn() throws InterruptedException {
+        System.out.println("observeOn()");
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        buildGenerator(100)
+                .doOnNext(debug("Generated\t"))
+                // observe in another thread, i.e. tell RxJava to process the values using one thread from the specified
+                // Scheduler
+                // NOTE: the position of this call DOES influence threading!
+                .observeOn(Schedulers.io())
+                .map(shiftUp).doOnNext(debug("Shifted Up\t"))
+                // from this observer onwards, observe in yet another thread
+                .observeOn(Schedulers.computation())
+                .map(shiftDown).doOnNext(debug("Shifted Down"))
+                .doOnCompleted(latch::countDown)
+                // subscribe on a different thread
+                // NOTE: the position of this call does NOT influence threading!
+                .subscribeOn(Schedulers.io())
+                .subscribe(debug("Received\t"));
+
+        latch.await();
+
+        System.out.println("");
+
+        return this;
+    }
+
+    private Threading delay() throws InterruptedException {
+        System.out.println("delay()");
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        buildGenerator(10)
+                .doOnNext(debug("Generated\t"))
+                .map(shiftUp).doOnNext(debug("Shifted Up\t"))
+                .delay(10, TimeUnit.MILLISECONDS).doOnNext(debug("Delayed"))
+                .map(shiftDown).doOnNext(debug("Shifted Down"))
+                .doOnCompleted(latch::countDown)
+                .subscribe(debug("Received\t"));
+
+        latch.await();
+
+        System.out.println("");
 
         return this;
     }
@@ -67,8 +122,10 @@ public class Threading {
     public static void main(String[] args) {
         try {
             new Threading()
-                    .noThreads()
-                    .subscribeOn_IOScheduler();
+                    /*.noThreads()
+                    .subscribeOn()
+                    .observeOn()*/
+                    .delay();
 
         } catch (InterruptedException e) {
             e.printStackTrace();
