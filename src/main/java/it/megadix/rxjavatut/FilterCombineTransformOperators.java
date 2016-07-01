e35d0e31e1f35c7bc96accecc4077ada44d23b0c
Dimitri De Franciscis <dimitri@megadix.it>
2016-07-01 10:52:30 +0200

    Better FilterCombineTransformOperators.java

--- src/main/java/it/megadix/rxjavatut/FilterCombineTransformOperators.java
+++ src/main/java/it/megadix/rxjavatut/FilterCombineTransformOperators.java
@@ -5,6 +5,8 @@ import rx.Observable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.function.Function;
+import java.util.function.IntFunction;
 
 import static it.megadix.rxjavatut.Utils.*;
 
@@ -12,44 +14,50 @@ public class FilterCombineTransformOperators {
 
     private long maxSleep = 300;
 
-    private Observable<Integer> createNumbersObservable(int num) {
+    private <R> Observable<R> createObservable(int num, IntFunction<R> supplier) {
+
         return Observable.create(subscriber ->
                 new Thread(() -> {
                     for (int i = 0; i < num; i++) {
                         if (subscriber.isUnsubscribed()) {
                             return;
                         }
-                        subscriber.onNext(i);
+
+                        R result = supplier.apply(i);
+
+                        subscriber.onNext(result);
                         randomSleep(maxSleep);
                     }
 
                     if (!subscriber.isUnsubscribed()) {
                         subscriber.onCompleted();
                     }
+
                 }).start()
         );
     }
 
-    private Observable<Character> createCharactersObservable(int num) {
-        return Observable.create(subscriber ->
-                new Thread(() -> {
-                    for (int i = 0; i < num; i++) {
-                        if (subscriber.isUnsubscribed()) {
-                            return;
-                        }
-                        subscriber.onNext((char) (i + 97));
-                        randomSleep(maxSleep);
-                    }
+    /**
+     * Create an {@link Observable} that produces <code>num</code> {@link Integer}s.
+     *
+     * @param num
+     */
+    private Observable<Integer> createNumbersObservable(int num) {
+        return createObservable(num, i -> i);
+    }
 
-                    if (!subscriber.isUnsubscribed()) {
-                        subscriber.onCompleted();
-                    }
-                }).start()
-        );
+    /**
+     * Create an {@link Observable} that produces <code>num</code> {@link Character}s.
+     *
+     * @param num
+     */
+    private Observable<Character> createCharactersObservable(int num) {
+        return createObservable(num, i -> (char) (i + 97));
     }
 
     private FilterCombineTransformOperators example_skip_take_map_reduce() {
 
+        System.out.println("--------------------------------------------------");
         System.out.println("Example: from(), skip(), take(), map(), reduce()");
 
         List<Integer> items = new ArrayList<>(100);
@@ -73,14 +81,15 @@ public class FilterCombineTransformOperators {
 
     private FilterCombineTransformOperators example_merge() throws InterruptedException {
 
+        System.out.println("--------------------------------------------------");
         System.out.println("Example: merge()");
+        System.out.println("Output of both observers is interleaved, i.e. depends on the scheduling of threads of the" +
+                " OS & JVM in use");
 
         CountDownLatch latch = new CountDownLatch(1);
 
         Observable<Integer> numbers = createNumbersObservable(10);
         Observable<Character> letters = createCharactersObservable(10);
-
-        // output of both observers is interleaved, i.e. depends on order of arrival
 
         Observable
                 .merge(numbers, letters)
@@ -105,14 +114,15 @@ public class FilterCombineTransformOperators {
 
     private FilterCombineTransformOperators example_concat() throws InterruptedException {
 
+        System.out.println("--------------------------------------------------");
         System.out.println("Example: concat()");
+        System.out.println("- output of both observers is concatenated, NOT interleaved");
+        System.out.println("- the second observable intervenes only after the first calls subscriber.onCompleted()");
 
         CountDownLatch latch = new CountDownLatch(1);
 
         Observable<Integer> numbers = createNumbersObservable(10);
         Observable<Character> letters = createCharactersObservable(10);
-
-        // output of both observers is concatenated, NOT interleaved
 
         Observable
                 .concat(numbers, letters)
@@ -137,7 +147,9 @@ public class FilterCombineTransformOperators {
 
     private FilterCombineTransformOperators example_flatMap() throws InterruptedException {
 
+        System.out.println("--------------------------------------------------");
         System.out.println("Example: flatMap()");
+        System.out.println("Note that flatMap() internally uses merge(), so output could be interleaved");
 
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -145,8 +157,6 @@ public class FilterCombineTransformOperators {
         numbers.add(createNumbersObservable(5));
         numbers.add(createNumbersObservable(5));
         numbers.add(createNumbersObservable(5));
-
-        // flatMap() internally uses merge(), so output can be interleaved
 
         Observable
                 .from(numbers)
@@ -172,7 +182,9 @@ public class FilterCombineTransformOperators {
 
     private FilterCombineTransformOperators example_concatMap() throws InterruptedException {
 
+        System.out.println("--------------------------------------------------");
         System.out.println("Example: concatMap()");
+        System.out.println("Note that concatMap() internally uses concat(), so output is NOT interleaved");
 
         CountDownLatch latch = new CountDownLatch(1);
 
@@ -180,8 +192,6 @@ public class FilterCombineTransformOperators {
         numbers.add(createNumbersObservable(5));
         numbers.add(createNumbersObservable(5));
         numbers.add(createNumbersObservable(5));
-
-        // concatMap() internally uses concat(), so output is NOT interleaved
 
         Observable
                 .from(numbers)
